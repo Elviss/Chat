dc5c1bd5c9ad65ba40f6b6fdd8c6b397c1ac6301
Elvis <slv_evs@yahoo.com.br>
2018-04-07 02:52:03 -0300

    finalizacao da classe servidor do novo exemplo

--- dev/Servidor.java
+++ dev/Servidor.java
@@ -85,4 +85,30 @@ public class Servidor extends Thread {
         }
 
     }
+
+    public void main(String[] args){
+
+        try {
+            //Cria os objetos necessário para instânciar o servidor
+            JLabel lblMessage = new JLabel("Porta do Servidor:");
+            JTextField txtPorta = new JTextField("12345");
+            Object[] texts = {lblMessage, txtPorta };
+            JOptionPane.showMessageDialog(null, texts);
+            server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
+            clientes = new ArrayList<BufferedWriter>();
+            JOptionPane.showMessageDialog(null,"Servidor ativo na porta: "+
+                    txtPorta.getText());
+
+            while (true) {
+                System.out.println("Aguardando conexão...");
+                Socket con = server.accept();
+                System.out.println("Cliente conectado...");
+                Thread t = new Servidor(con);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
